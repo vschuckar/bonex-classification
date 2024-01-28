dce74984f64da0b7448e37d64d0bb50da5a23db9
vitoria <vschuckar@gmail.com>
2024-01-29 00:09:01 +0100

    Changes in the app layout

--- app/app.py
+++ app/app.py
@@ -6,12 +6,28 @@ import os
 
 image_path = os.path.join(os.path.dirname(__file__), 'analytica.png')
 st.sidebar.image(image_path, use_column_width=True)
-st.title("boneX")
-st.subheader("The newest Image Analytica product!")
-st.text("boneX is a x-ray fracture classifier."
-        "In this first app interaction, boneX classifies upper limbs fractures for its location. In the next releases, it will also find and mark the exact fracture location."
-            "Upload an X-ray image, and try it now!")
+st.sidebar.title("boneX")
+st.sidebar.subheader("The newest Image Analytica product!")
+#st.sidebar.text("boneX is a x-ray fracture classifier."
+#       "In this first app interaction, boneX classifies upper limbs fractures for its location. In the next releases, it will also find and mark the exact fracture location."
+#           "Upload an X-ray image, and try it now!")
 
+multiline_text = """
+### Overview
+
+boneX is a state-of-the-art X-ray fracture classifier. 
+In this app, you can upload an X-ray image of an upper limb, and boneX will analyze it to classify fractures in different locations.
+
+### How to Use
+
+1. Upload an X-ray image using the file uploader.
+2. Click the 'Make predictions!' button to analyze the image.
+3. boneX will provide predictions for various fracture locations.
+
+Enjoy using boneX for quick and accurate fracture classification!
+"""
+
+st.markdown(multiline_text, unsafe_allow_html=True)
 uploaded_file = st.file_uploader("Choose an X-ray image.", type=["jpg", "jpeg", "png"])
 
 
