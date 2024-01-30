0bd7bb3bf1b0c7414d17ba6dd8945ef9c08db31f
vitoria <vschuckar@gmail.com>
2024-01-30 09:43:51 +0100

    Changes to code to handle different colorscales

--- app/app_function.py
+++ app/app_function.py
@@ -1,14 +1,16 @@
 import pandas as pd 
 import numpy as np 
-
+import tensorflow as tf 
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.inception_v3 import preprocess_input
 
 
 def xray_image(img):
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)
+    '''
+    '''
+    img_rgb = tf.image.grayscale_to_rgb(image.img_to_array(img))
+    img_array = np.expand_dims(img_rgb, axis=0)
     img_array = preprocess_input(img_array)
 
     model = load_model('models/best_model_-10.h5')
